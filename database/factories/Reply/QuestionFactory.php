a976c878d2f954d8f72a944f7d244edf84df03f9
Roden Juanico <astraroden@gmail.com>
2024-07-09 16:12:52 +0800

    Creating Migration,Model,Factories

---
+++ database/factories/Reply/QuestionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Reply;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Reply\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2024_07_09_072006_create_replies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned;
+            $table->integer('user_id');
+
+            $table->foreign('question_id')->reference('id')>on('questions')
+            ->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('replies');
+    }
+};
